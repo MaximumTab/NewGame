c0fabde1fa2804936b1ae03dcfee29c189eeb83b
Jay <Byrnejay2001@gmail.com>
2025-09-07 14:48:19 +1000

    Fmod Build music implementation test

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
-using System.Numerics;
+using FMOD.Studio;
+
 
 public class AudioManager : MonoBehaviour
 {
+    private List<EventInstance> eventInstances;
+
+    private List<StudioEventEmitter> eventEmitters;
+    private EventInstance musicEventInstance;
+
+
     public static AudioManager instance { get; private set; }
 
     private void Awake()
@@ -13,12 +21,35 @@ public class AudioManager : MonoBehaviour
             Debug.LogError("found more than one AudioManager in the scene");
         }
         instance = this;
+        eventInstances = new List<EventInstance>();
+        eventEmitters = new List<StudioEventEmitter>();
+    }
+
+    private void Start()
+    {
+        InitializeMusic(FmodEvents.instance.music);
+    }
+    public EventInstance CreateInstance(EventReference eventReference)
+    {
+        EventInstance eventinstance = RuntimeManager.CreateInstance(eventReference);
+        eventInstances.Add(eventinstance);
+        return eventinstance;
+
+
+
     }
     public void PlayOneShot(EventReference sound)
     {
         RuntimeManager.PlayOneShot(sound);
     }
+
+    private void InitializeMusic(EventReference musicEventReference)
+    {
+        musicEventInstance = CreateInstance(musicEventReference);
+        musicEventInstance.start();
     }
+
+}
 
 
 
