b033e732c5404f3b12c7da391d3b3c41f74bc9b4
Jay <Byrnejay2001@gmail.com>
2025-09-07 14:57:56 +1000

    updated volume control into the project
    Added voulme control into the script

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -6,6 +6,20 @@ using FMOD.Studio;
 
 public class AudioManager : MonoBehaviour
 {
+    [Header("Volume")]
+    [Range(0, 1)]
+    
+    public float masterVolume = 1f;
+     [Range(0, 1)]
+    public float musicVolume = 1f;
+     [Range(0, 1)]
+    public float sfxVolume = 1f;
+    [Range(0, 1)]
+
+    private Bus masterBus;
+    private Bus musicBus;
+    private Bus sfxBus;
+
     private List<EventInstance> eventInstances;
 
     private List<StudioEventEmitter> eventEmitters;
@@ -23,11 +37,22 @@ public class AudioManager : MonoBehaviour
         instance = this;
         eventInstances = new List<EventInstance>();
         eventEmitters = new List<StudioEventEmitter>();
+
+        masterBus = RuntimeManager.GetBus("bus:/");
+        musicBus = RuntimeManager.GetBus("bus:/Music");
+        sfxBus = RuntimeManager.GetBus("bus:/SFX");
     }
 
     private void Start()
     {
         InitializeMusic(FmodEvents.instance.music);
+    }
+
+    private void Update()
+    {
+        masterBus.setVolume(masterVolume);
+        musicBus.setVolume(musicVolume);
+        sfxBus.setVolume(sfxVolume);
     }
     public EventInstance CreateInstance(EventReference eventReference)
     {
