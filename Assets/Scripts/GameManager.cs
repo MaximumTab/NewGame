9c39101d1ec234bc3185c22314dd9342423814ea
BDragonSoldier <ricky.oberholzer@gmail.com>
2025-09-08 12:45:26 +1000

    Endingstuffs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,11 +10,15 @@ public class GameManager : MonoBehaviour
     public int CurEnemyCount = 0;
     [SerializeField] private TMP_Text LivesDisplay;
     [SerializeField] private TMP_Text CounterDisplay;
+    private Animator ContrAnim;
+    private static readonly int Lose = Animator.StringToHash("Lose");
+    private static readonly int Win = Animator.StringToHash("Win");
 
 
     private void Start()
     {
         Time.timeScale = 1;
+        ContrAnim=gameObject.GetComponent<Animator>();
     }
 
     private void Update()
@@ -21,9 +26,14 @@ public class GameManager : MonoBehaviour
         if (Lives <= 0)
         {
             Debug.Log("You Failed");
-            Time.timeScale = 0;
+            ContrAnim.SetBool(Lose,true);
+            //Time.timeScale = 0;
+        }else if (IsWin())
+        {
+            Debug.Log("You Win");
+            ContrAnim.SetBool(Win,true);
         }
-        
+
         LivesDisplay.text = ""+Lives;
         CounterDisplay.text = CurEnemyCount + "/" + AllEnemyCount;
     }
@@ -31,6 +41,21 @@ public class GameManager : MonoBehaviour
     public void SetCurEnemCount()
     {
         CurEnemyCount++;
+    }
+
+    public bool IsWin()
+    {
+        if (CurEnemyCount == AllEnemyCount)
+        {
+            EnemyBehaviour[] allEnems = FindObjectsByType<EnemyBehaviour>(FindObjectsInactive.Include,FindObjectsSortMode.None);
+            if (allEnems.Length==0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+
     }
 
     public void SetEnemyCount(int Count)
