59e81a02ce2ac0f2e83c11bf21ec9e5cf495c56f
Jay <Byrnejay2001@gmail.com>
2025-10-05 21:41:59 +1100

    healer ability cooldown added to not overflow

--- Assets/Scripts/AbilityTypes/HealerAbil.cs
+++ Assets/Scripts/AbilityTypes/HealerAbil.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using FMODUnity;
 
 [CreateAssetMenu(fileName = "AbilityHeal", menuName = "Abilities/Healing")]
 public class HealerAbil : Ability
 {
-    public override void UseAbility(GameObject Target, Vector3 Source,float Atk)
+    [Header("SFX Cooldown (seconds)")]
+    public float healSFXCooldown = 10f;
+    public float lastHealSFXTime = -999f;
+    public override void UseAbility(GameObject Target, Vector3 Source, float Atk)
     {
-        GameObject newProj=Instantiate(Projectile, Source, Quaternion.identity);
+        if (!abilitySFX.IsNull && Time.unscaledTime - lastHealSFXTime >= healSFXCooldown)
+        {
+            RuntimeManager.PlayOneShot(abilitySFX);
+            lastHealSFXTime = Time.unscaledTime;
+        }
+
+        GameObject newProj = Instantiate(Projectile, Source, Quaternion.identity);
         if (newProj.GetComponent<ProjectileBehaviour>())
         {
             newProj.GetComponent<ProjectileBehaviour>().DamageDoneTo(-Atk, AtkModifier, Target);
